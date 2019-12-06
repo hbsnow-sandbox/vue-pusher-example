06fcb3c723295146eb9736ad67215a5133f5cdc8
hbsnow <yuki.4uing@gmail.com>
2019-12-06 09:10:11 +0900

    init

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount('#app')
+
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import VueChatScroll from 'vue-chat-scroll'
+
+import App from './App.vue'
+import router from './router'
+import store from './store/index'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/css/loading.css'
+import './assets/css/loading-btn.css'
+
+Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(VueChatScroll)
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount('#app')
